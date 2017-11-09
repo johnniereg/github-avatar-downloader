9af3d22247bf189a557b2d2614ac329c4fb664a6
Johnnie Regalado <johnniereg@gmail.com>
2017-11-09 00:42:36 +0000

    Function now grabbing starred repos and adding them to an object to hold onto them.

--- recommend.js
+++ recommend.js
@@ -10,8 +10,7 @@ var fs = require('fs');
 var path = require('path');
 
 
-// Given a repo owner and repo name, return an object where each item is an ob.
-function getRepoContributors(repoOwner, repoName) {
+function recommendRepos(repoOwner, repoName) {
   const options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers : {
@@ -20,24 +19,77 @@ function getRepoContributors(repoOwner, repoName) {
     }
   };
 
+  const allStarredRepos = {
+
+  }
+
+  // Request information from a given repo and return JSON object of contributors
   request(options, function(err, res, body) {
     let contributors = JSON.parse(body);
     if (contributors.message) {
       throw (contributors.message);
     }
-    // for (let contributor in contributors) {
-    //   let user = contributors[contributor];
-    //   console.log(user.starred_url);
-    // }
-    return contributors;
+
+    // Loop over the contributors to the repo and access their starred repos
+    for (let contributor in contributors) {
+
+      let user = contributors[contributor];
+      let login = user.login;
+      let starredURL = user.starred_url;
+      options.url = `https://api.github.com/users/${login}/starred`;
+
+      request(options, function(err, res, body) {
+        let starredRepos = JSON.parse(body);
+
+        for (let entry in starredRepos) {
+          let repoName = starredRepos[entry].full_name;
+
+          if (allStarredRepos[repoName] === undefined) {
+            allStarredRepos[repoName] = 1;
+          } else {
+            allStarredRepos[repoName] += 1;
+          }
+
+        console.log("***Starts here.");
+        console.log(allStarredRepos);
+        console.log("***Ends here.");
+        }
+
+
+
+      });
+
+    }
+    // return contributors;
   });
+
+
+
+
 }
 
-function getStarredRepos(contributors) {
+recommendRepos("lighthouse-labs", "laser_shark");
 
 
-}
+// let contributors = getRepoContributors("lighthouse-labs", "laser_shark");
 
+// function getStarredRepos(contributors) {
 
-getRepoContributors("lighthouse-labs", "laser_shark");
+//   for (let contributor in contributors) {
+
+//     let username = contributors[contributor].login;
+//     console.log(username);
+
+//     const options = {
+//       url: `https://api.github.com/users/${username}/starred`,
+//       headers : {
+//         'User-Agent': 'request',
+//         'Authorization': 'token ' + process.env.GITHUB_API_TOKEN
+//       }
+//     };
+
+//   }
+
+// }
+
 
